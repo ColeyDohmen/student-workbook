03a0c60410460d73b582e7529cb3e3f39c392179
ColeyDohmen <coley.dawn12@gmail.com>
2021-04-05 13:20:57 -0600

    C#rockpaperscissors

---
+++ csharp/rpscissors/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace rpscissors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
